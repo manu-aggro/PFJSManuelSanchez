9e36a55c3a8c2646b61b2fcdab87881b509fed45
Manuel Sanchez <manu.sierranorte@gmail.com>
2024-07-07 18:26:54 +0200

    Vamos con try y catch

--- app/index.js
+++ app/index.js
@@ -1,59 +1,106 @@
 
 let largo;
 let ancho;
+let area;
+let numerodetomateras;
+let numerodeberenjenas;
+let numerodecalabazas;
+let numerodelechugas;
+let terrenoocupado = 0;
+let terrenoocupado2 = 0;
+let terrenoocupado3 = 0;
+let terrenoocupado4 = 0;
 
-function capturaDatos(){
+function capturaDatos() {
   largo = parseInt(document.getElementById("largo").value);
- ancho = parseInt(document.getElementById("ancho").value);
+  ancho = parseInt(document.getElementById("ancho").value);
+  numerodetomateras = parseInt(document.getElementById("tomateras").value);
+  numerodeberenjenas = parseInt(document.getElementById("berenjenas").value);
+  numerodecalabazas = parseInt(document.getElementById("calabazas").value);
+  numerodelechugas = parseInt(document.getElementById("lechugas").value);
+
 }
 
 function dibujarHuerto() {
-    
-    var body = document.getElementsByTagName("body")[0];
-  
-    var tabla = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    for (var i = 0; i < ancho; i++) {
-      var hilera = document.createElement("tr");
-      
-  
-      for (var j = 0; j < largo; j++) {
-        var celda = document.createElement("td");
-        celda.id=`${i}-${j}`
-        hilera.appendChild(celda);
-      }
-  
-      tblBody.appendChild(hilera);
+
+  var body = document.getElementsByTagName("body")[0];
+
+  var tabla = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  for (var i = 0; i < ancho; i++) {
+    var hilera = document.createElement("tr");
+
+
+    for (var j = 0; j < largo; j++) {
+      var celda = document.createElement("td");
+      celda.id = `${i * ancho + j}`
+      hilera.appendChild(celda);
     }
-  
-    tabla.appendChild(tblBody);
-    body.appendChild(tabla);
-    const h3 = document.getElementById("subtitulo");
-    const form = document.getElementById("form");
-    h3.remove();
-    form.remove();
+
+    tblBody.appendChild(hilera);
   }
-  
-function guardarDatos(){
-  
+
+  tabla.appendChild(tblBody);
+  body.appendChild(tabla);
+  const h3 = document.getElementById("subtitulo");
+  const form = document.getElementById("form");
+  h3.remove();
+  form.remove();
+}
+
+function guardarDatos() {
+
   localStorage.setItem("Largo del huerto", largo);
   localStorage.setItem("Ancho del huerto", ancho);
   localStorage.setItem("Largo del huerto", area);
+  localStorage.setItem("Numero de tomateras", numerodetomateras);
+  localStorage.setItem("Numero de berenjenas", numerodeberenjenas);
+  localStorage.setItem("Numero de calabazas", numerodeberenjenas);
+  localStorage.setItem("Numero de lechugas", numerodelechugas);
 
 }
 
-function plantar(){
-  document.getElementById("1-3").style.backgroundImage = "url(../assets/tomatoe.jpg)"
+function plantar() {
+
+  for (let index = 0; index < 3 * numerodetomateras; index += 3) {
+    document.getElementById(`${index}`).style.backgroundImage = "url(../assets/tomate-soil.jpg)";
+    terrenoocupado = 3 * numerodetomateras
+
+
+  }
+  for (let index = 0; index < 3 * numerodeberenjenas; index += 3) {
+    document.getElementById(`${index + terrenoocupado}`).style.backgroundImage = "url(../assets/berenjena-soil.png)";
+
+    terrenoocupado2 = 3 * numerodeberenjenas + terrenoocupado
+
+  }
+  for (let index = 0; index < 4 * numerodecalabazas; index += 4) {
+    document.getElementById(`${index + terrenoocupado2}`).style.backgroundImage = "url(../assets/calabaza-soil.png)";
+
+    terrenoocupado3 = 4 * numerodecalabazas + terrenoocupado2
+
+  }
+  for (let index = 0; index < 1 * numerodelechugas; index += 1) {
+    document.getElementById(`${index + terrenoocupado3}`).style.backgroundImage = "url(../assets/lechuga-soil.png)";
+
+
+
+  }
+
+
+
 }
 
 
 function start() {
-   capturaDatos();
-    dibujarHuerto();
-    plantar();
-    guardarDatos();
+  capturaDatos();
+  dibujarHuerto();
+  plantar();
+  guardarDatos();
 
 }
+
+
 
 
